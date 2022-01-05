f5e700b2da6e6d5275204263bd969cbc94ba8c37
anastasiyarzhevutskaya <hallygirl@mail.ru>
2022-01-05 14:27:15 +0300

    add: tasks 1 - 5 solutions

--- src/tasks/3.ts
+++ src/tasks/3.ts
@@ -8,3 +8,8 @@
 // All numbers are valid Int32, no need to validate them.
 // There will always be at least one number in the input string.
 // Output string must be two numbers separated by a single space, and highest number is first.
+
+export const maxAndMinSymbols = (string: string): string => {
+    const numberArray = string.split(' ').map((str) => Number(str));
+    return `${Math.max(...numberArray)} ${Math.min(...numberArray)}`;
+}
